026234015b1666818ee3a91ae6cf520e2b43c064
Pranay <praveenpranay07@gmail.com>
2026-01-07 11:02:27 +0530

    Implemented  get blogs API to return all blogs from database.

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,3 +1,9 @@
-exports.getBlogs = (req, res, next) => {
-    res.json({ message: "Blogs Fetched Successfully" }); 
-};
+exports.getBlogs = async (req, res, next) => {
+    try {
+        const blogs = await Blog.find();
+        res.status(200).json({status: "success", blogs});
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}; 
